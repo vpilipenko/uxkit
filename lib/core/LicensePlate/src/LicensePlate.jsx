e065af62549f361084105b6e99de67ed118b4032
V.Pilipenko <v.pilipenko@avtopass.net>
2020-12-08 18:20:41 +0300

    custom styling and optimization

--- lib/core/LicensePlate/src/LicensePlate.jsx
+++ lib/core/LicensePlate/src/LicensePlate.jsx
@@ -1,6 +1,6 @@
 import cm from './LicensePlate.module.styl'
 
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import PropTypes from 'prop-types'
 import cx from 'classnames'
@@ -8,212 +8,235 @@ import cx from 'classnames'
 import toCyrillic from './utils'
 
 
-class LicensePlate extends Component {
+const LicensePlate = ({
+  value,
+  type,
+  isValid,
+  id,
+  size,
+  classes,
+  invalidText,
+}) => {
+  const [initialValue, setInitialValue] = useState('')
+  const [initialType, setInitialType] = useState('')
+  const [isParsed, setIsParsed] = useState(false)
 
-  static propTypes = {
-    /** Component ID in DOM */
-    id: PropTypes.string,
-    /** Serie, number, region */
-    value: PropTypes.string.isRequired,
-    /** Plate type */
-    type: PropTypes.oneOf(['type1', 'type1b', 'type2']),
-    /** Size of component */
-    size: PropTypes.oneOf(['s', 'm', 'l']),
-    /** isValid */
-    isValid: PropTypes.func,
-    /** Invalid text */
-    invalidText: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.func,
-    ]),
-  }
-
-  static defaultProps = {
-    size: 'm',
-  }
-
-  constructor(props, context) {
-    super(props, context)
-    const { value, type, isValid } = this.props
-    const v = this.getValue(value)
-    const t = type || this.getType(v)
-
+  useEffect(() => {
+    if (!isParsed) {
+      setIsParsed(true)
+    }
+    const v = parseValue(value)
+    const t = type || parseType(v)
     isValid && isValid(!!t, t, v)
-
-    this.state = {
-      value: v,
-      type: t,
-    }
-  }
+    setInitialType(t)
+    setInitialValue(v)
+  }, [isValid, value, type])
 
 
-  componentDidUpdate(prevProps, prevState) {
-    const { value, type, isValid } = this.props
-
-    if (prevProps.value !== value || prevProps.type !== type) {
-      const v = this.getValue(value)
-      const t = type || this.getType(v)
-
-      isValid && isValid(!!t, t, v)
-
-      this.setState({
-        value: v,
-        type: t,
-      })
-    }
-  }
+  const { number, region } = getResult(initialType, initialValue)
 
 
-  getValue = value => {
-    if (!value) { return '' }
-    value = value.toUpperCase()
-    value = value.replace(/\s/g, '')
-    value = toCyrillic(value)
-    return value
-  }
+  if (!isParsed) { return null }
 
 
-  getType = value => {
-    if (!value) {
-      return ''
-    }
-
-    // Тип 1 -> Регистрационные знаки легковых, грузовых автомобилей и автобусов
-    const type1 = /^[ABEKMHOPCTYXАВЕКМНОРСТУХ]{1}\d{3}[ABEKMHOPCTYXАВЕКМНОРСТУХ]{2}\d{2,3}$/g
-    // Тип 1Б -> Регистрационные знаки для легковых такси, транспортных средств, оборудованных для перевозок более восьми человек, осуществляющих перевозку на основании лицензии
-    const type1b = /^[ABEKMHOPCTYXАВЕКМНОРСТУХ]{2}\d{5}$/g
-    // Тип 2 -> Регистрационные знаки для автомобильных прицепов и полуприцепов.
-    const type2 = /^[ABEKMHOPCTYXАВЕКМНОРСТУХ]{2}\d{6}$/g
-
-    if (type1.test(value)) {
-      return 'type1'
-    }
-
-    if (type1b.test(value)) {
-      return 'type1b'
-    }
-
-    if (type2.test(value)) {
-      return 'type2'
-    }
-
-    return ''
-  }
-
-  renderType1 = ({ value, id, size }) => {
-    value = value.match(/^[ABEKMHOPCTYXАВЕКМНОРСТУХ]{1}|\d{3}|[ABEKMHOPCTYXАВЕКМНОРСТУХ]{2}|\d{2,3}/g) || []
-    const serieStart = value[0]
-    const number = value[1]
-    const serieEnd = value[2]
-    const region = value[3]
-
-    return (
-      <div id={id} className={cx(cm.license_plate, {
-        [cm[`size-${size}`]]: !!size,
-      })}>
-        <span className={cm.main}>
-          {`${serieStart} ${number} ${serieEnd}`}
-        </span>
-        <span className={cm.region}>
-          {region}
-        </span>
-      </div>
-    )
-  }
-
-  renderType1b = ({ value, id, size }) => {
-    value = value.match(/^[ABEKMHOPCTYXАВЕКМНОРСТУХ]{2}|\d{3}|\d+/g) || []
-    const serie = value[0]
-    const number = value[1]
-    const region = value[2]
-
-    return (
-      <div id={id} className={cx(cm.license_plate, cm.yellow, {
-        [cm[`size-${size}`]]: !!size,
-      })}>
-        <span className={cm.main}>
-          {`${serie} ${number}`}
-        </span>
-        <span className={cm.region}>
-          {region}
-        </span>
-      </div>
-    )
-  }
-
-  renderType2 = ({ value, id, size }) => {
-    value = value.match(/^[ABEKMHOPCTYXАВЕКМНОРСТУХ]{2}|\d{4}|\d+/g) || []
-    const serie = value[0]
-    const number = value[1]
-    const region = value[2]
-
-    return (
-      <div id={id} className={cx(cm.license_plate, {
-        [cm[`size-${size}`]]: !!size,
-      })}>
-        <span className={cm.main}>
-          {`${serie} ${number}`}
-        </span>
-        <span className={cm.region}>
-          {region}
-        </span>
-      </div>
-    )
-  }
-
-  renderInvalid = ({ value, id, size }) => {
-    const { invalidText } = this.props
-
+  if (!number && !region) {
     let text = invalidText
+
     if (typeof(invalidText) === 'function') {
-      text = invalidText({ value })
+      text = invalidText({ value: initialValue })
     }
 
     return (
-      <div id={id} className={cx(cm.license_plate, {
-        [cm[`size-${size}`]]: !!size,
+      <div id={id} className={cx({
+        [cm.license_plate]: true,
+        [cm.invalid]: true,
+        [classes.invalid]: !!classes.invalid,
       })}>
-        <span className={cm.main}>
-          {text || value}
-        </span>
+        {text}
       </div>
     )
   }
 
-  getPlate = ({ type, value }) => {
-    const { id, size } = this.props
 
-    switch (type) {
-      case 'type1':
-        return (
-          this.renderType1({ value, id, size })
-        )
+  return (
+    <div id={id} className={cx({
+      [cm.root]: true,
+      [cm[`size-${size}`]]: !!size,
+      [classes.root]: !!classes.root,
+    })}>
+      <If condition={number}>
+        <span className={cx({
+          [cm.number]: true,
+          [classes.number]: !!classes.number,
+        })}>
+          {number}
+        </span>
+      </If>
+      <If condition={region}>
+        <span className={cx({
+          [cm.region]: true,
+          [classes.region]: !!classes.region,
+        })}>
+          {region}
+        </span>
+      </If>
+    </div>
+  )
+}
 
-      case 'type1b':
-        return (
-          this.renderType1b({ value, id, size })
-        )
-
-      case 'type2':
-        return (
-          this.renderType2({ value, id, size })
-        )
-
-      default:
-        return (
-          this.renderInvalid({ value, id, size })
-        )
-    }
-  }
-
-
-  render() {
-    const { value, type } = this.state
-
-    return (
-      this.getPlate({ value, type })
-    )
-  }
+LicensePlate.propTypes = {
+  /** Component ID in DOM */
+  id: PropTypes.string,
+  /** Serie, number, region */
+  value: PropTypes.string.isRequired,
+  /** Plate type */
+  type: PropTypes.oneOf(['type1', 'type1b', 'type2']),
+  /** Size of component */
+  size: PropTypes.oneOf(['s', 'm', 'l']),
+  /** isValid */
+  isValid: PropTypes.func,
+  /** Invalid text */
+  invalidText: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.func,
+  ]),
+  /** Additional classes */
+  classes: PropTypes.object,
+}
+LicensePlate.defaultProps = {
+  size: 'm',
+  invalidText: 'Invalid value',
+  classes: {
+    root: '',
+    number: '',
+    region: '',
+    invalid: '',
+  },
 }
 
 export default LicensePlate
+
+
+const getResult = (type, value) => {
+  let result = {
+    number: '',
+    region: '',
+  }
+
+  if (!type || !value) {
+    return result
+  }
+
+  switch (type) {
+    case 'type1':
+      result = resultType1(value)
+      break
+    case 'type1b':
+      result = resultType1b(value)
+      break
+    case 'type2':
+      result = resultType2(value)
+      break
+  }
+
+  return result
+}
+
+
+const typeCache = {}
+// Тип 1 -> Регистрационные знаки легковых, грузовых автомобилей и автобусов
+const type1 = /^[ABEKMHOPCTYXАВЕКМНОРСТУХ]{1}\d{3}[ABEKMHOPCTYXАВЕКМНОРСТУХ]{2}\d{2,3}$/g
+// Тип 1Б -> Регистрационные знаки для легковых такси, транспортных средств, оборудованных для перевозок более восьми человек, осуществляющих перевозку на основании лицензии
+const type1b = /^[ABEKMHOPCTYXАВЕКМНОРСТУХ]{2}\d{5}$/g
+// Тип 2 -> Регистрационные знаки для автомобильных прицепов и полуприцепов.
+const type2 = /^[ABEKMHOPCTYXАВЕКМНОРСТУХ]{2}\d{6}$/g
+
+const parseType = string => {
+  if (!string) { return '' }
+  if (typeCache[string]) { return typeCache[string] }
+
+  if (type1.test(string)) {
+    typeCache[string] = 'type1'
+    return 'type1'
+  }
+
+  if (type1b.test(string)) {
+    typeCache[string] = 'type1b'
+    return 'type1b'
+  }
+
+  if (type2.test(string)) {
+    typeCache[string] = 'type2'
+    return 'type2'
+  }
+
+  return ''
+}
+
+
+const valueCache = {}
+const parseValue = string => {
+  if (!string) { return '' }
+  if (valueCache[string]) { return valueCache[string]}
+
+  let value = string.toUpperCase()
+  value = value.replace(/\s/g, '')
+  value = toCyrillic(value)
+
+  valueCache[string] = value
+  return value
+}
+
+const resultCache = {}
+const resultType1 = string => {
+  if (resultCache[string]) { return resultCache[string] }
+
+
+  const value = string.match(/^[ABEKMHOPCTYXАВЕКМНОРСТУХ]{1}|\d{3}|[ABEKMHOPCTYXАВЕКМНОРСТУХ]{2}|\d{2,3}/g) || []
+  const serieStart = value[0]
+  const number = value[1]
+  const serieEnd = value[2]
+  const region = value[3]
+
+  const result = {
+    number: `${serieStart} ${number} ${serieEnd}`,
+    region,
+  }
+
+  resultCache[string] = result
+  return result
+}
+
+const resultType1b = string => {
+  if (resultCache[string]) { return resultCache[string] }
+
+  const value = string.match(/^[ABEKMHOPCTYXАВЕКМНОРСТУХ]{2}|\d{3}|\d+/g) || []
+  const serie = value[0]
+  const number = value[1]
+  const region = value[2]
+
+  const result = {
+    number: `${serie} ${number}`,
+    region,
+  }
+
+  resultCache[string] = result
+  return result
+}
+
+const resultType2 = string => {
+  if (resultCache[string]) { return resultCache[string] }
+
+  const value = string.match(/^[ABEKMHOPCTYXАВЕКМНОРСТУХ]{2}|\d{4}|\d+/g) || []
+  const serie = value[0]
+  const number = value[1]
+  const region = value[2]
+
+  const result = {
+    number: `${serie} ${number}`,
+    region,
+  }
+
+  resultCache[string] = result
+  return result
+}
